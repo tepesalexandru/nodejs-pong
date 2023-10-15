c3a1773002e68cce5f91689d79088c63bf489f8c
Tepes Alexandru <tepes_alexandru@outlook.com>
2023-10-15 20:42:39 +0300

    changed variable type to be matrix

--- index.js
+++ index.js
@@ -4,17 +4,58 @@ const gridWidth = 60;
 
 // Variable to hold the grid
 const generateGrid = () => {
-  let grid = "";
-  grid += "#".repeat(gridWidth) + "\n";
-  for (let i = 0; i < gridHeight - 2; i++) {
-    grid += "#" + " ".repeat(gridWidth - 2) + "#" + "\n";
+  let grid = [];
+  for (let i = 0; i < gridHeight; i++) {
+    grid.push([]);
+    if (i === 0 || i === gridHeight - 1) {
+      for (let j = 0; j < gridWidth; j++) {
+        grid[i].push("#");
+      }
+    } else {
+      grid[i].push("#");
+      for (let j = 0; j < gridWidth - 2; j++) {
+        grid[i].push(" ");
+      }
+      grid[i].push("#");
+    }
   }
-  grid += "#".repeat(gridWidth) + "\n";
   return grid;
 };
 
-printGrid = () => {
-  console.log(generateGrid());
+printGrid = (grid) => {
+  for (let i = 0; i < gridHeight; i++) {
+    console.log(grid[i].join(""));
+  }
 };
 
-printGrid();
+const movePlayer = (grid) => {
+  const newPosition = {
+    x: playerPosition.x + 1,
+    y: playerPosition.y,
+  };
+  console.log(newPosition);
+  // Remove the player from the grid
+  grid = grid.replace("O", " ");
+  // Update the player position
+  playerPosition.x = newPosition.x;
+  playerPosition.y = newPosition.y;
+  // Add the player to the grid
+  // grid = grid.substr(0, playerPosition.y) + "O" + grid.substr(playerPosition.y);
+  grid[gridWidth * playerPosition.x + playerPosition.y] = "O";
+  printGrid(grid);
+};
+
+const playerPosition = {
+  x: 1,
+  y: 1,
+};
+
+// const gameLoop = () => {
+//   let grid = generateGrid();
+//   printGrid(grid);
+//   movePlayer(grid);
+//   setTimeout(gameLoop, 1000);
+// };
+
+// gameLoop();
+printGrid(generateGrid());

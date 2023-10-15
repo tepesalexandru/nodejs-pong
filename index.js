0e1e53e6b8fb09324b5913bded25d6201196e06b
Tepes Alexandru <tepes_alexandru@outlook.com>
2023-10-15 20:19:24 +0300

    basic grid draw

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("This is Pong!");
+
+// Create a 10 x 10 grid
+const gridHeight = 20;
+const gridWidth = 60;
+
+console.log("#".repeat(gridWidth));
+for (let i = 0; i < gridHeight - 2; i++) {
+  console.log("#" + " ".repeat(gridWidth - 2) + "#");
+}
+console.log("#".repeat(gridWidth));
